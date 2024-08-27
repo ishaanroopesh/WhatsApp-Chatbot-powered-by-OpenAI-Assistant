a55824232fd0b2171c8f051b6d48949226697afc
Ishaan Roopesh <86351273+ishaanroopesh@users.noreply.github.com>
2024-08-27 15:55:51 +0530

    Update send.py

--- send.py
+++ send.py
@@ -1,12 +1,13 @@
 from twilio.rest import Client
 
-
+account_sid = #Account ID
+auth_token = #Authorisation token here
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
   from_='whatsapp:+14155238886',
   body='Your appointment is coming up on July 21 at 3PM',
-  to='whatsapp:+919902685178'
+  to='whatsapp:+XXXXXXXXXXXX' #ex :- +911234567890
 )
 
 print(message.sid)
